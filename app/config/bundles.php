5a505554034be1ddf8bc4ae8f7248eae530127e5
damir <dv19751217@gmail.com>
2025-03-04 08:09:05 +0300

    Добавлен NelmioApiDocBundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
